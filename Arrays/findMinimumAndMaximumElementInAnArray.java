c977eafc42d082381742fce8bf3648ed3269cdb6
Abhay Deshmukh <abhaydeshmukh@Abhays-MacBook-Air.local>
2022-08-01 12:23:28 +0530

    findMinimumAndMaximumElementInAnArray.java

---
+++ Arrays/findMinimumAndMaximumElementInAnArray.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+class pair {
+    long first;
+    long second;
+
+    public pair(long a, long b) {
+        this.first = a;
+        this.second = b;
+    }
+
+}
+
+class Compute {
+
+    static pair getMinMax(long a[], long n) {
+        // Write your code here
+        long min = a[0];
+        long max = a[0];
+
+        for (int i = 1; i < n; i++) {
+            long val = (long) a[i];
+            if (val > max) {
+                max = val;
+            }
+            if (val < min) {
+                min = val;
+            }
+        }
+        return new pair(min, max);
+    }
+}
